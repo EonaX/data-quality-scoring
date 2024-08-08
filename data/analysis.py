6b5ed202b42f00c4216b59f7e6a4210bc3aef060
max-bld <mblanchard95@gmail.com>
2024-08-08 15:52:55 +0200

    schema analysis

--- data/analysis.py
+++ data/analysis.py
@@ -16,15 +16,33 @@ for n in range(len(apidae_data['objetsTouristiques'])):
  
 print(f'Unique types of objetsTouristiques:\n{set(list_types)}')
 
-#%%
+#%% data
 
-list_keys = []
+# types and number of fields per objetsTouristiques
 
 for n in range(len(apidae_data['objetsTouristiques'])):
-    list_keys.append(list(apidae_data['objetsTouristiques'][n].keys()))
     print("Objet Touristique: {apidae_data['objetsTouristiques'][n]['nom']['libelleFr']}")
     print(f"Type: {apidae_data['objetsTouristiques'][n]['type']}")
     print(f"Nombre de champs:{len(apidae_data['objetsTouristiques'][n])}\n")
-    
 
-print(set(list_keys))
+#%% schemas
+
+# types of objetsTouristiques
+
+print("Types d'objets touristiques modélisés:\n")
+
+for n in range(len(apidae_schemas['properties']['objetsTouristiques']['items'])):
+    print(apidae_schemas['properties']['objetsTouristiques']['items'][n]['id'])
+
+# properties per objetsTouristiques
+
+print("Propriétés par types d'objets touristiques modélisés:\n")
+
+for n in range(len(apidae_schemas['properties']['objetsTouristiques']['items'])):   
+    
+    print(apidae_schemas['properties']['objetsTouristiques']['items'][n]['id'])
+    key = input('Enter [y] to display its properties, otherwise enter any other key.\n')
+    
+    if key == 'y':
+            pprint(apidae_schemas['properties']['objetsTouristiques']['items'][n]['properties'], depth = 1)
+
