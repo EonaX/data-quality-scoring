a1dea83ae35408b1ee2e3799a6fd66a6827bbb54
max-bld <mblanchard95@gmail.com>
2024-08-08 15:30:40 +0200

    load schemas

--- data/load.py
+++ data/load.py
@@ -6,16 +6,20 @@ Created on Thu Aug  8 14:47:01 2024
 @author: maximeb
 """
 
-
 import json
 import requests
+
 # with Postman
 
 with open('datasets/vaut_le_detour.json') as f:
     apidae_data = json.load(f)
 
-# Get the schemas
+#%% Download the schemas
 
 r = requests.get('https://github.com/apidae-tourisme/apidae-sit-schemas/raw/main/v002/api/output/apiObjetsTouristiquesResultat.schema')
+
 with open(r'schemas/apiObjetsTouristiquesResultat.schema', 'wb') as f:
     f.write(r.content)
+
+with open('schemas/apiObjetsTouristiquesResultat.schema') as f:
+    apidae_schemas = json.load(f)
