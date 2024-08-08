f206965e5923bc3afdf93356e7f0fc5de1b6b7f9
max-bld <mblanchard95@gmail.com>
2024-08-08 15:19:06 +0200

    data from apidae (dataset+schema) and python scripts to analyze it

---
+++ data/analysis.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 14:47:50 2024
+
+@author: maximeb
+"""
+
+from data.load import apidae_data
+from pprint import pprint
+
+list_types = []
+
+for n in range(len(apidae_data['objetsTouristiques'])):
+    list_types.append(apidae_data['objetsTouristiques'][n]['type'])
+ 
+print(f'Unique types of objetsTouristiques:\n{set(list_types)}')
+
+#%%
+
+list_keys = []
+
+for n in range(len(apidae_data['objetsTouristiques'])):
+    list_keys.append(list(apidae_data['objetsTouristiques'][n].keys()))
+    print("Objet Touristique: {apidae_data['objetsTouristiques'][n]['nom']['libelleFr']}")
+    print(f"Type: {apidae_data['objetsTouristiques'][n]['type']}")
+    print(f"Nombre de champs:{len(apidae_data['objetsTouristiques'][n])}\n")
+    
+
+print(set(list_keys))
---
+++ data/load.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 14:47:01 2024
+
+@author: maximeb
+"""
+
+
+import json
+import requests
+# with Postman
+
+with open('datasets/vaut_le_detour.json') as f:
+    apidae_data = json.load(f)
+
+# Get the schemas
+
+r = requests.get('https://github.com/apidae-tourisme/apidae-sit-schemas/raw/main/v002/api/output/apiObjetsTouristiquesResultat.schema')
+with open(r'schemas/apiObjetsTouristiquesResultat.schema', 'wb') as f:
+    f.write(r.content)
