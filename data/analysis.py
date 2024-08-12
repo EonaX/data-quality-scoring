0f033219bce69f4c4b1c6ffa5b49981ebe76bc80
max-bld <mblanchard95@gmail.com>
2024-08-12 12:04:40 +0200

    functions added

--- data/analysis.py
+++ data/analysis.py
@@ -6,83 +6,153 @@ Created on Thu Aug  8 14:47:50 2024
 @author: maximeb
 """
 
-from load import apidae_data, apidae_schemas
 from pprint import pprint
-import matplotlib.pyplot as plt
 
-list_types = []
+def print_list_types(data):
+    """
+    Returns the set of unique types of objetsTouristiques.
 
-for n in range(len(apidae_data['objetsTouristiques'])):
-    list_types.append(apidae_data['objetsTouristiques'][n]['type'])
- 
-print(f'Unique types of objetsTouristiques:\n{set(list_types)}')
+    Parameters
+    ----------
+    data : TYPE
+        DESCRIPTION.
 
-#%% DATA ANALYSIS
-#%%% types and number of fields per objetsTouristiques
+    Returns
+    -------
+    None.
 
-for n in apidae_data['objetsTouristiques']:
-    print("Objet Touristique:", n['nom']['libelleFr'])
-    print("Type:", n['type'])
-    print("Nombre de champs:", len(n), "\n")
-
-#%% SCHEMAS ANALYSIS
-#%%% types of objetsTouristiques
-
-print("Types d'objets touristiques modélisés:\n")
-
-for n in apidae_schemas['properties']['objetsTouristiques']['items']:
-    print('Type:', n['id'])
-    print('Nombre de champs:', len(n['properties'].keys()), '\n')
+    """
+    list_types = []
     
-#%%% properties per objetsTouristiques
+    for n in range(len(data['objetsTouristiques'])):
+        list_types.append(data['objetsTouristiques'][n]['type'])
+     
+    print(f'Unique types of objetsTouristiques:\n{set(list_types)}')
 
-print("Propriétés par types d'objets touristiques modélisés:\n")
+def print_infos_data(data):
+    """
+    types and number of fields per objetsTouristiques
 
-for n in range(len(apidae_schemas['properties']['objetsTouristiques']['items'])):   
+    Parameters
+    ----------
+    data : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    for n in data['objetsTouristiques']:
+        print("Objet Touristique:", n['nom']['libelleFr'])
+        print("Type:", n['type'])
+        print("Nombre de champs:", len(n), "\n")
+
+
+def count_objects_per_type(schema):
+    """
+    Count objetsTouristiques number per types in the schema.
+
+    Parameters
+    ----------
+    schema : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    print("Types d'objets touristiques modélisés:\n")
+
+    for n in schema['properties']['objetsTouristiques']['items']:
+        print('Type:', n['id'])
+        print('Nombre de champs:', len(n['properties'].keys()), '\n')
+
+def print_properties_per_type(schema):
+    """
+    Print properties per type of objetsTouristiques present in the schema.
+
+    Parameters
+    ----------
+    schema : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    print("Propriétés par types d'objets touristiques modélisés:\n")
     
-    print(apidae_schemas['properties']['objetsTouristiques']['items'][n]['id'])
-    key = input('Enter [y] to display its properties, otherwise enter any other key.\n')
-    
-    if key == 'y':
-            pprint(apidae_schemas['properties']['objetsTouristiques']['items'][n]['properties'], depth = 1)
-
-#%%% shared properties between all objetsTouristiques
-
-l = []
-
-for n in apidae_schemas['properties']['objetsTouristiques']['items']:  
-    l.append(set(list(n['properties'].keys())))
-    
-common_fields = (set.intersection(*l))
-
-print('Champs communs à tous les objets touristiques:\n', common_fields)
-
-#%% DATA COMPLETENESS AGAINST SCHEMAS
-
-# Common fields present in each object
-
-count = {}
-
-for n in common_fields:
-    count[n] = 0
-
-for n in apidae_data['objetsTouristiques']:
-    s = set.intersection(common_fields, set(list(n.keys())))
-    for m in s:
-        count[m]= count[m] + 1
+    for n in range(len(schema['properties']['objetsTouristiques']['items'])):   
         
-# percent
+        print(schema['properties']['objetsTouristiques']['items'][n]['id'])
+        key = input('Enter [y] to display its properties, otherwise enter any other key.\n')
+        
+        if key == 'y':
+                pprint(schema['properties']['objetsTouristiques']['items'][n]['properties'], depth = 1)
 
-for n in count:
-    count[n] = count[n]/len(apidae_data['objetsTouristiques'])*100
+def print_common_properties(schema, print_results=False):
 
-#%% bar chart visualization
+    """
+    Returns the intersection set of properties between all objetsTouristisques types.
 
-fig = plt.figure(figsize=(19.2, 10.8))
-plt.barh(*zip(*count.items()))
-plt.title('Completeness Percentage of Common Fields in the Dataset', size=36)
-plt.ylabel('Common Fields to All Types', size=24)
-plt.xlabel('percent %', size=24)
-#plt.xticks(rotation=45)
-plt.grid(axis='x')
-plt.show()
+    Parameters
+    ----------
+    schema : TYPE
+        DESCRIPTION.
+        
+    print_results : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    common_fields : TYPE
+        DESCRIPTION.
+
+    """
+    l = []
+    
+    for n in schema['properties']['objetsTouristiques']['items']:  
+        l.append(set(list(n['properties'].keys())))
+        
+    intersection_properties = (set.intersection(*l))
+    if print_results == True:
+        print('Champs communs à tous les objets touristiques:\n', intersection_properties)
+    
+    return intersection_properties
+
+def get_completeness_percent(data, fields):
+    """
+    Returns the completeness percent of the data depending on the presence or absence of the given fields.
+
+    Parameters
+    ----------
+    data : TYPE
+        DESCRIPTION.
+    fields : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    completeness_percent = {}
+    
+    for n in fields:
+        completeness_percent[n] = 0
+    
+    for n in data['objetsTouristiques']:
+        s = set.intersection(fields, set(list(n.keys())))
+        for m in s:
+            completeness_percent[m]= completeness_percent[m] + 1
+            
+    # percent
+    
+    for n in completeness_percent:
+        completeness_percent[n] = completeness_percent[n]/len(data['objetsTouristiques'])*100
+        
+    return completeness_percent
--- data/load.py
+++ data/load.py
@@ -7,19 +7,40 @@ Created on Thu Aug  8 14:47:01 2024
 """
 
 import json
-import requests
 
+def open_data(path):
+    with open(path) as f:
+        return json.load(f)
+    
 # with Postman
 
-with open('data/datasets/vaut_le_detour.json') as f:
-    apidae_data = json.load(f)
+
+# with requests
+
+# # url = "https://api.plateforme.apidae-tourisme.com/api/v002/channel/recherche/list-objets-touristiques"
+
+# # params = {
+# #     "apiKey":"f4238d9a-e5ab-43eb-ada9-3d2e0230ec19",
+# #     "projectId":"29e03482-980b-4d37-b25b-dc88705a578e",
+# #     "searchId":"4609b3d7-e375-47cf-8a4c-a5a20ee68abc",
+# #     "count": 100
+# #     }
+
+# # r = requests.get(url, params)
 
 #%% Download the schemas
 
-# r = requests.get('https://github.com/apidae-tourisme/apidae-sit-schemas/raw/main/v002/api/output/apiObjetsTouristiquesResultat.schema')
+# with requests
 
-# with open(r'data/schemas/apiObjetsTouristiquesResultat.schema', 'wb') as f:
-#     f.write(r.content)
+# # r = requests.get('https://github.com/apidae-tourisme/apidae-sit-schemas/raw/main/v002/api/output/apiObjetsTouristiquesResultat.schema')
 
-with open('data/schemas/apiObjetsTouristiquesResultat.schema') as f:
-    apidae_schemas = json.load(f)
+# # with open(r'data/schemas/apiObjetsTouristiquesResultat.schema', 'wb') as f:
+# #     f.write(r.content)
+
+def open_schemas(path):
+
+    with open('data/schemas/apiObjetsTouristiquesResultat.schema') as f:
+        return json.load(f)
+    
+#%%
+
